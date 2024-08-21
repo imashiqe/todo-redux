66bff6b0250b3c2e79060bc9fde84f45bc53b5ff
Ashiqur Rahman <imashiqe@gmail.com>
2024-08-21 17:03:29 +0600

    add shad cdn redux and configure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./index.css";
-
+import { Button } from "./components/ui/button";
 function App() {
   return (
     <div>
-      <h1 className="bg-green-400">Hello World</h1>
+      <h1 className="bg-green-400">My Todo</h1>
+      <Button>My Button</Button>
     </div>
   );
 }
